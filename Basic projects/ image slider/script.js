3eb798af0f3830a39e79e0a27dfc6ad6e6695800
Ekram🦋 <145211609+ikramdev101@users.noreply.github.com>
2024-07-04 14:57:19 +0200

    Create script.js

---
+++ Basic projects/ image slider/script.js
@@ -0,0 +1,27 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    if (index >= totalSlides) {
+        currentIndex = 0;
+    } else if (index < 0) {
+        currentIndex = totalSlides - 1;
+    } else {
+        currentIndex = index;
+    }
+    const offset = -currentIndex * 100;
+    document.querySelector('.slides').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+    showSlide(currentIndex - 1);
+}
+
+
+
+
