ec1977b6ff098a370fa3f8783507c2fa581e2992
Ekram🦋 <145211609+ikramdev101@users.noreply.github.com>
2024-07-05 23:59:41 +0200

    Update script.js

--- Basic projects/ image slider/script.js
+++ Basic projects/ image slider/script.js
@@ -1,25 +1,25 @@
-let currentIndex = 0
+let Index = 0
 const slides = document.querySelectorAll('.slide')
 const totalSlides = slides.length
 
 function showSlide(index) {
     if (index >= totalSlides) {
-        currentIndex = 0;
+ Index = 0;
     } else if (index < 0) {
         currentIndex = totalSlides - 1
     } else {
-        currentIndex = index
+        Index = index
     }
-    const offset = -currentIndex * 100
+    const offset = -Index * 100
     document.querySelector('.slides').style.transform = `translateX(${offset}%)`
 }
 
 function nextSlide() {
-    showSlide(currentIndex + 1)
+    showSlide(Index + 1)
 }
 
 function prevSlide() {
-    showSlide(currentIndex - 1)
+    showSlide(Index - 1)
 }
 
 
