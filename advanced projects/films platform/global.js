2d056697b1fb82fef5926cbd5b65ccf2ad59b43e
Ekram🦋 <145211609+ikramdev101@users.noreply.github.com>
2024-08-23 18:21:42 +0200

    Update global.js

--- advanced projects/films platform/global.js
+++ advanced projects/films platform/global.js
@@ -5,6 +5,7 @@ const addEventOnElements=function (elements,eventType,callback){
     for(const elem of elements) elem.addEventListener(eventType,callback)
 }
 //we used attributs to call elements wit querySelector
+// wa added some interactives to the search barr
 const searchbox= document.querySelector('[search-box]')
 const searchTogglers=document.querySelectorAll('[search-toggler]')
  addEventOnElements(searchTogglers,'click',function(){
