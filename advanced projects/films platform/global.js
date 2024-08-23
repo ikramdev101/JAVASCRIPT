3134394833b90fe2c8494406f402b5356ef93f0b
Ekram🦋 <145211609+ikramdev101@users.noreply.github.com>
2024-08-23 18:21:09 +0200

    Update global.js

--- advanced projects/films platform/global.js
+++ advanced projects/films platform/global.js
@@ -1,8 +1,10 @@
-'use strict'
+'use strict' //just pretend like it's a safe mpde for the code 
+// general fun allows u to add same evenet to multip elements 
 
 const addEventOnElements=function (elements,eventType,callback){
     for(const elem of elements) elem.addEventListener(eventType,callback)
 }
+//we used attributs to call elements wit querySelector
 const searchbox= document.querySelector('[search-box]')
 const searchTogglers=document.querySelectorAll('[search-toggler]')
  addEventOnElements(searchTogglers,'click',function(){
