893259c688e79e2ef74e71f2c9af83e84b22827c
Ekram🦋 <145211609+ikramdev101@users.noreply.github.com>
2024-08-27 16:14:15 +0200

    Update global.js

--- advanced projects/films platform/global.js
+++ advanced projects/films platform/global.js
@@ -2,7 +2,7 @@
 
 
 
-jgjn,ko, sibzn iuibuvfcr 
+
 // general fun allows u to add same evenet to multip elements 
 const addEventOnElements=function (elements,eventType,callback){
     for(const elem of elements) elem.addEventListener(eventType,callback)
