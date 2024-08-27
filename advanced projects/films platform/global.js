4cb22caf0c5cfae35fd11ea2590444b575a84318
Ekram🦋 <145211609+ikramdev101@users.noreply.github.com>
2024-08-27 16:13:10 +0200

    Update global.js

--- advanced projects/films platform/global.js
+++ advanced projects/films platform/global.js
@@ -1,6 +1,9 @@
-'use strict' //just pretend like it's a safe mpde for the code 
-// general fun allows u to add same evenet to multip elements 
+'use strict' //just pretend like it's a safe mode for the code 
 
+
+
+
+// general fun allows u to add same evenet to multip elements 
 const addEventOnElements=function (elements,eventType,callback){
     for(const elem of elements) elem.addEventListener(eventType,callback)
 }
